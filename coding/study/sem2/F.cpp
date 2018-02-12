d04c52bb0b794b06a48d7065fdde3796a8a41042
kvk1920 <kvk1901@yandex.ru>
2018-02-12 23:41:39 +0300

    debugger

--- coding/study/sem2/F.cpp
+++ coding/study/sem2/F.cpp
@@ -4,296 +4,99 @@ using namespace std;
 
 typedef long long ll;
 
-struct node
+#define DEBUG
+
+#ifdef DEBUG
+struct
 {
-	/* base */
-	node *L, *R; //children
-
-	ll x; //data
-	int y; //priority
-	int sz; //size
-
-	/* assign */
-	bool need_assign;
-
-	/* add */
-	ll add;
-
-	/* sum */
-	ll sum;
-
-	/* permutations */
-	bool need_reverse;
-	bool no_dec, no_inc; //inc_eq, dec_eq
-	ll l, r; //left and right elements of segment
-
-	node(ll x) : x(x)
+	vector <ll> b;
+	ll sum(int l, int r)
 	{
-		L = R = 0;
-		y = rand();
-		sz = 1;
-
-		need_assign = false;
-
-		add = 0;
-
-		sum = x;
-
-		need_reverse = false;
-		no_dec = no_inc = true;
-		l = r = x;
+		ll res(0);
+		for (int i(l); i <= r; ++i)
+			res += b[i];
+		return res;
 	}
-
-	void update_assign(ll new_x)
+	void insert(ll x, int pos)
 	{
-		l = r = x = new_x;
-		sum = new_x * sz;
-		add = 0;
-		need_assign = true;
-		no_dec = no_inc = true;
+		b.push_back(0);
+		for (int i(b.size() - 1); i > pos; --i)b[i] = b[i - 1];
+		b[pos] = x;
 	}
-
-	void update_add(ll k)
+	void erase(int pos)
 	{
-		l += k, r += k;
-		x += k;
-		sum += k * sz;
-		add += k;
+		for (int i(pos); i < b.size() - 1; ++i)
+			b[i] = b[i + 1];
+		b.pop_back();
 	}
-
-	void update_reverse()
+	void assign(ll x, int l, int r)
 	{
-		need_reverse ^= 1;
-		swap(L, R);
-		swap(no_inc, no_dec);
-		swap(l, r);
+		for (int i(l); i <= r; ++i)
+			b[i] = x;
 	}
-
-	void push()
+	void add(ll x, int l, int r)
 	{
-		if (need_assign)
-		{
-			if (L) L->update_assign(x);
-
-			if (R) R->update_assign(x);
-
-			need_assign = false;
-		}
-
-		if (need_reverse)
-		{
-			if (L) L->update_reverse();
-
-			if (R) R->update_reverse();
-
-			need_reverse = false;
-		}
-
-		if (add)
-		{
-			if (L) L->update_add(add);
-
-			if (R) R->update_add(add);
-
-			add = 0;
-		}
+		for (int i(l); i <= r; ++i)
+			b[i] += x;
 	}
-
-	void recalc()
+	void next_perm(int l, int r)
 	{
-		sz = 1;
-		l = r = x;
-		sum = x;
-		no_inc = no_dec = true;
-
-		if (L)
-		{
-			sz += L->sz;
-			sum += L->sum;
-			l = L->l;
-			no_inc &= L->r >= x && L->no_inc;
-			no_dec &= L->r <= x && L->no_dec;
-		}
-
-		if (R)
-		{
-			sz += R->sz;
-			sum += R->sum;
-			r = R->r;
-			no_inc &= R->l <= x && R->no_inc;
-			no_dec &= R->l >= x && R->no_dec;
-		}
+		next_permutation(b.begin() + l, b.begin() + r + 1);
 	}
-};
+	void prev_perm(int l, int r)
+	{
+		prev_permutation(b.begin() + l, b.begin() + r + 1);
+	}
+} debugger;
 
-typedef node *treap;
-typedef pair <treap, treap> ptt;
-
-inline int sz(treap T) { return T ? T->sz : 0; }
-
-ptt split(treap T, int key)
+ll do_operation(int t, int x, int l, int r, int pos)
 {
-	if (!T) return { 0, 0 };
-
-	T->push();
-
-	if (sz(T->L) + 1 <= key)
-	{
-		ptt tmp(split(T->R, key - 1 - sz(T->L)));
-
-		T->R = tmp.first;
-		T->recalc();
-
-		return { T, tmp.second };
-	}
-	else
-	{
-		ptt tmp(split(T->L, key));
-
-		T->L = tmp.second;
-		T->recalc();
-
-		return { tmp.first, T };
-	}
+	if (t == 1) return debugger.sum(l, r);
+	if (t == 2) debugger.insert(x, pos);
+	if (t == 3) debugger.erase(pos);
+	if (t == 4) debugger.assign(x, l, r);
+	if (t == 5) debugger.add(x, l, r);
+	if (t == 6) debugger.next_perm(l, r);
+	if (t == 7) debugger.prev_perm(l, r);
 }
 
-treap merge(treap L, treap R)
+#endif
+
+int main()
 {
-	if (!L || !R) return L ? L : R;
-
-	L->push();
-	R->push();
-
-	if (L->y > R->y)
+#ifdef DEBUG
+	freopen("debug", "w", stdout);
+#else
+	freopen("output.txt", "w", stdout);
+#endif
+	freopen("input.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	int input[n];
+	for (int i(0); i < n; ++i) cin >> input[i];
+#ifdef DEBUG
+	for (int i(0); i < n; ++i)
+		debugger.b.push_back(input[i]);
+#endif
+	int q;
+	cin >> q;
+	ll res;
+	for (int t, x, l, r, pos; q; --q)
 	{
-		L->R = merge(L->R, R);
-		L->recalc();
-
-		return L;
+		cin >> t;
+		if (t == 1 || t == 6 || t == 7) cin >> l >> r;
+		else if (t == 3) cin >> pos;
+		else if (t == 2) cin >> x >> pos;
+		else cin >> x >> l >> r;
+#ifdef DEBUG
+		res = do_operation(t, x, l, r, pos);
+		if (t == 1) cout << res << endl;
+#endif
 	}
-	else
-	{
-		R->L = merge(L, R->L);
-		R->recalc();
-
-		return R;
-	}
-}
-
-template <typename T, typename ...arg_list>
-T merge(T a, arg_list b) { return merge(a, merge(b...)); }
-
-inline void split_segment(treap T, treap &L, treap &M, treap &R, int l, int r)
-{
-	ptt tmp1(split(T, r));
-	ptt tmp2(split(tmp1.first, l - 1));
-	L = tmp2.first;
-	R = tmp1.second;
-	M = tmp2.second;
-}
-
-inline ll sum_query(treap &T, int l, int r)
-{
-	treap L, M, R;
-	split_segment(T, L, R, M, l, r);
-	ll res(M->sum);
-	T = merge(L, M, R);
-	return res;
-}
-
-inline void insert_query(treap &T, ll x, int pos)
-{
-	ptt tmp(split(T, pos - 1));
-	T = merge(tmp.first, new node(x), tmp.second);
-}
-
-/*
-void destruct(treap T)
-{
-	if (!T) return;
-	destruct(T->L);
-	destruct(T->R);
-	delete T;
-}
-*/
-
-inline void erase_query(treap &T, int pos)
-{
-	treap L, M, R;
-	split_segment(T, L, M, R, pos, pos);
-	T = merge(L, R);
-	//destruct(M);
-}
-
-inline void assign_query(treap &T, int l, int r, ll x)
-{
-	treap L, M, R;
-	split_segment(T, L, M, R, l, r);
-	M->update_assign(x);
-	T = merge(L, M, R);
-}
-
-inline void add_query(treap &T, int l, int r, ll x)
-{
-	treap L, M, R;
-	split_segment(T, L, M, R, l, r);
-	M->update_add(x);
-	T = merge(L, M, R);
-}
-
-int get_no_inc_suff_len(treap T) // But it calc pref...
-{
-	int sum(0);
-	while (T && !T->no_inc)
-	{
-		if (!T->R->no_inc)
-		{
-			sum += sz(T->L) + 1;
-			T = T->R;
-		}
-		else if (T->R->no_inc)
-			if (T->x < T->R->l)
-			{
-				sum += 1 + sz(T->L);
-				T = T->R;
-			}
-			else T = T->L;
-	}
-	return sum;
-}
-
-int get_pos_greater(treap T, int k)
-{
-	while (true)
-		if (T->R && T->R->l > k)
-			T = T->R;
-		else if (T->x > k)
-			return T->x;
-		else
-			T = T->L;
-}
-
-int get_pos_less(treap T, int k)
-{
-	while (true)
-		if (T->R && T->R->l < k)
-			T = T->R;
-		else if (T->x < k)
-			return T->x;
-		else
-			T = T->L;
-}
-
-inline void next_permutation(treap &T, int l, int r)
-{
-	treap L, M, R;
-	split_segment(T, L, M, R, l, r);
-	if (M->no_inc)
-	{
-		M->update_reverse();
-		T = merge(L, M, R);
-		return;
-	}
-	int pref_len = get_no_inc_suff_len(M);
-	ptt tmp(split(M, pref_len - 1));
+#ifdef DEBUG
+	for (ll i : debugger.b) cout << i << ' ';
+#endif
 }
