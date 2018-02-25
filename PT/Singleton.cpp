b0ed1d8f1e369873372769e64953bd9aedbc0a52
kvk1920 <34111966+kvk1920@users.noreply.github.com>
2018-02-26 00:17:33 +0300

    singleton

---
+++ PT/Singleton.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Singletone
+{
+public:
+	friend Singletone &make_Singletone();
+
+private:
+	Singletone()
+	{
+		ofstream fout("__singletone__file__");
+		fout.write(reinterpret_cast<char *>(&int(this)), sizeof (int));
+		fout.close();
+	}
+
+	Singletone(const Singletone &) {}
+	Singletone &operator=(const Singletone &) {}
+};
+
+inline Singletone &make_Singletone()
+{
+	ifstream fin("__singletone__file__");
+	if (!fin.is_open())
+	{
+		ofstream fout("__singletone__file__");
+		fout.write(reinterpret_cast <char *>(&int(new Singletone)), sizeof (int));
+		fout.close();
+		fin.open("__singletone__file__");
+	}
+	Singletone *ptr;
+	int input;
+	fin.read(reinterpret_cast <char *>(&input), sizeof (int));
+	ptr = input;
+	fin.close();
+	return *ptr;
+}
